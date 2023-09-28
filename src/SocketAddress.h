2b9daf8927cfef41edb38fbe12e4b13932a512eb
bloodmask <kiremate@hotmail.com>
2023-09-28 12:56:09 +0200

    Created IPV4 and IPV6 initial wrappers

--- src/SocketAddress.h
+++ src/SocketAddress.h
@@ -3,10 +3,31 @@
 #include <cstdint>
 class SocketAddress {
 public:
-	SocketAddress(uint32_t inAddress, uint16_t inPort) {
-		
+	SocketAddress(uint32_t inAddress, uint16_t inPort) { // IPV4 Constructor
+		GetAsSockAddrIn()->sin_family = AF_INET;
+		GetAsSockAddrIn()->sin_addr.S_un.S_addr = htonl(inAddress);
+		GetAsSockAddrIn()->sin_port = htons(inPort);
+	}
+	SocketAddress(const uint8_t inAddress[16], uint16_t inPort) { // IPV6 Constructor
+		GetAsSockAddrIn6()->sin6_family = AF_INET6;
+		memcpy(&GetAsSockAddrIn6()->sin6_addr, inAddress, 16);
+		GetAsSockAddrIn6()->sin6_port = htons(inPort);
+	}
+	SocketAddress(const sockaddr& inSockAddr) { // Copy Constructor
+		memcpy(&mSockAddr, &inSockAddr, sizeof(inSockAddr));
 	}
 
 private:
-	sockaddr mSockAddr;
+	union {
+		sockaddr mSockAddr;
+		sockaddr_in mSockAddr4;
+		sockaddr_in6 mSockAddr6;
+	};
+	
+	sockaddr_in* GetAsSockAddrIn() {
+		return &mSockAddr4;
+	}
+	sockaddr_in6* GetAsSockAddrIn6() {
+		return &mSockAddr6;
+	}
 };
