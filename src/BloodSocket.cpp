995fb177cce3bc272d2d4b21d4e1854a541d26b3
bloodmask <kiremate@hotmail.com>
2023-10-15 00:27:50 +0200

    Added the Macros for Linux

--- src/BloodSocket.cpp
+++ src/BloodSocket.cpp
@@ -2,19 +2,33 @@
 #include "SocketUtil.h"
 int BloodSocket::SetNonBlockingMode(bool inShouldBeNonBlocking)
 {
-	#if defined (_WIN32) || defined(__CYGWIN)// Windows x64 - x86
-		u_long arg = inShouldBeNonBlocking ? 1 : 0;
-		int result = ioctlsocket(mSocket, FIONBIO, &arg);
-	#else
-		int flags = fcntl(mSocket, F_GETFL, 0);
-		flags = inShouldBeNonBlocking ?
-			(flags | O_NONBLOCK) : (flags & ~O_NONBLOCK);
-		fcntl(mSocket, F_SETFL, flags);
-	#endif
-		if (result == SOCKET_ERROR) {
-			SocketUtil::ReportError(L"UDPSocket::SetNonBlockingMode");
-			return SocketUtil::GetLastError();
-		}
-		else
-			return NO_ERROR;
+    int result = 0;  // Initialize result to a neutral value
+
+    #if defined(_WIN32) || defined(__CYGWIN) // Windows x64 - x86
+        u_long arg = inShouldBeNonBlocking ? 1 : 0;
+        result = ioctlsocket(mSocket, FIONBIO, &arg);
+    #else // Linux/Unix
+        int flags = fcntl(mSocket, F_GETFL, 0);
+        flags = inShouldBeNonBlocking ? (flags | O_NONBLOCK) : (flags & ~O_NONBLOCK);
+        result = fcntl(mSocket, F_SETFL, flags);
+
+    // If fcntl failed, set result to SOCKET_ERROR
+    if (result == -1) {
+        result = SOCKET_ERROR;
+    }
+    #endif
+
+    // Check for any errors
+    if (result == SOCKET_ERROR) {
+        SocketUtil::ReportError(L"BloodSocket::SetNonBlockingMode");
+        return SocketUtil::GetLastError();
+    }
+    else {
+        // No errors, so return NO_ERROR or its equivalent
+    #if defined(_WIN32) || defined(__CYGWIN)
+        return NO_ERROR;
+    #else
+        return 0;  // Equivalent of NO_ERROR in Linux/Unix
+    #endif
+    }
 }
--- src/PlatformSocketIncludes.h
+++ src/PlatformSocketIncludes.h
@@ -1,19 +1,28 @@
 #pragma once
 #if defined(_WIN32) || defined(__CYGWIN)// Windows x64 - x86
-	#define WIN32_LEAN_AND_MEAN  // Exclude rarely-used stuff from Windows headers
-	#include <WinSock2.h>
-	#include <WS2tcpip.h>
-	#include <Windows.h>
+#define WIN32_LEAN_AND_MEAN  // Exclude rarely-used stuff from Windows headers
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <Windows.h>
 #elif defined(__linux__) // Linux
-	#include <sys/socket.h>
-	#include <netinet/in.h>
-	#include <netdb.h>
-	#include <fcntl.h>  
-	typedef int SOCKET;
-	#define INVALID_SOCKET -1
-	#define SOCKET_ERROR -1
-#elif defined(unix) || defined(__unix__) || defined(__unix) // Unix like OS like PS4/5
-	#include <sys/socket.h>
-	#include <netinet/in.h>
-	#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <fcntl.h>
+typedef int SOCKET;
+#define INVALID_SOCKET -1
+
+// Define SOCKET_ERROR and NO_ERROR if not already defined
+#ifndef SOCKET_ERROR
+#define SOCKET_ERROR -1
+#endif
+
+#ifndef NO_ERROR
+#define NO_ERROR 0
+#endif
+#elif defined(unix) || defined(__unix__) || defined(__unix) // Unix-like OS
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+// ... and so on
 #endif
