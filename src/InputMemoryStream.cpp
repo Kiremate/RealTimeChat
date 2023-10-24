f4dbf881266bfabf6d846c9e881f8278ff381ff2
bloodmask <kiremate@hotmail.com>
2023-10-24 22:53:46 +0200

    InputMemory Stream for input data reading

---
+++ src/InputMemoryStream.cpp
@@ -0,0 +1,14 @@
+#include "InputMemoryStream.h"
+#include <stdexcept>
+
+void InputMemoryStream::Read(void* outData, uint32_t inByteCount)
+{
+	if ((mHead + inByteCount) > mCapacity)
+		throw std::runtime_error("Not enough bytes left in stream to perform read operation");
+	
+	std::memcpy(outData, mBuffer + mHead, inByteCount);
+	mHead += inByteCount;
+}
+void InputMemoryStream::ResetToZero() {
+	mHead = 0;
+}
---
+++ src/InputMemoryStream.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <cstdint>
+#include <memory>
+class InputMemoryStream{
+
+public:
+	InputMemoryStream(char* inBuffer, uint32_t inByteCount, bool ownsBuffer = false): 
+		mBuffer(inBuffer), mCapacity(inByteCount), mHead(0), mOwnsBuffer(ownsBuffer) {}
+	~InputMemoryStream() {
+		if(mOwnsBuffer)
+			std::free(mBuffer);
+	}
+	uint32_t GetRemainingDataSize() const { return mCapacity - mHead; }
+	void Read(void* outData, uint32_t inByteCount);
+	void Read(uint32_t& outData) { Read(&outData, sizeof(outData)); }
+	void Read(int32_t& outData) { Read(&outData, sizeof(outData)); }
+private:
+	bool mOwnsBuffer;
+	void ResetToZero();
+	char* mBuffer;
+	uint32_t mHead;
+	uint32_t mCapacity;
+};
